28e54fa5a2a0951279eaa12e079bffc5bcdec018
Irina Haidarova <irish114899@gmail.com>
2025-04-18 09:10:05 +0300

    create GET /health/

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Привет Otus!"))
+	w.WriteHeader(http.StatusOK) // одно и тоже что и 200
+	w.WriteHeader(200)
+}
+
+func handler2(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "PUT" {
+		w.WriteHeader(http.StatusMethodNotAllowed) // вернем 405
+		return
+	}
+	w.Write([]byte("Привет!"))
+	w.WriteHeader(http.StatusOK) // одно и тоже что и 200
+}
 
 func main() {
-	fmt.Println(`I like Go!
-I like Go!
-I like Go!`)
+	http.HandleFunc("/health/", handler)
+	http.HandleFunc("/2", handler2)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
