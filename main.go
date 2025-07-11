7b28373de7a26fa8d41139e34e7d50083d89faca
Irina Haidarova <irish114899@gmail.com>
2025-07-11 11:51:19 +0300

    delete pod settings & nginx settings fix

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func handler2(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/health/", handler)
-	http.HandleFunc("/2", handler2)
+	http.HandleFunc("/", handler2)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		fmt.Println("Ошибка запуска сервера:", err)
